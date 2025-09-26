1222d967a7df3a408344d07332540415e3fc9bab
vishnuvarthanssviswa-programer <vishnuvarthanssviswa@gmail.com>
2025-09-26 12:37:49 +0530

    Update Mapview.tsx

--- src/components/Mapview.tsx
+++ src/components/Mapview.tsx
@@ -1,40 +1,21 @@
-import React from 'react';
+// src/components/MapView.tsx
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
 
-// Fix marker icons (Leaflet expects images)
-// This ensures icons load properly when bundling
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+const position: [number, number] = [9.9312, 76.2673]; // Coordinates for Kochi, Kerala
 
-interface MapViewProps {
-  center?: [number, number];
-  zoom?: number;
-}
-
-const MapView: React.FC<MapViewProps> = ({
-  center = [10.8505, 76.2711],
-  zoom = 7,
-}) => {
+function MapView() {
   return (
-    <div className="w-full h-96">  {/* e.g. height 24rem */}
-      <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/ url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={center}>
-          <Popup>
-            Kerala Center
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          Kochi, Kerala
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
-};
-export default MapView;
+}
+export default MapView;
